f2f13daa13f562b1a555afa230a126c3659f198a
MarioBroi <inoix16@gmail.com>
2024-04-17 14:12:16 +0100

    add variable discs in script.php Filled the discs variable with the content of the scihi.json file, using the file_get_content function, then checked the content of the variable with a var_dump

--- assets/helpers/script.php
+++ assets/helpers/script.php
@@ -1,2 +1,8 @@
 <?php
-echo "Test script php";
+//echo "Test script php";
+
+//save in the variable discs all the array of dischi.json 
+$discs = file_get_contents('dischi.json');
+
+//check the content of the discs variable
+var_dump($discs);
