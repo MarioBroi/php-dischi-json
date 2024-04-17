16ed5c46b5a5ae3cfe2093011b59b5685c0b5dee
MarioBroi <inoix16@gmail.com>
2024-04-17 16:14:52 +0100

    edit script.php, app.js and index.php Moved script.php in the main folder. Add app.js axios call to catch the script.php data Add a v-for cycle in index.php to dynamically generate the albums cards with image and name

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
-console.log('test');
+console.log('app test');
 
 const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-            
+            albums: [],
+            error: '',
         }
     },
     methods: {
@@ -13,5 +14,23 @@ createApp({
     },
     mounted() {
 
-    }
+        //faccio una chiamata axios
+        axios
+
+            //vado a prendere l'api url 
+            .get('script.php')
+
+            //controllo che risultato mi dá
+            .then((result) => {
+                console.log(result.data);
+                this.albums = result.data;
+                console.log(this.albums);
+            })
+
+            //catturo errori in caso ci siano
+            .catch((err) => {
+                console.log(err.message);
+                this.error = err.message
+            });
+    },
 }).mount('#app');
--- index.php
+++ index.php
@@ -1,37 +1,40 @@
-<?php
-include_once __DIR__ . '/assets/helpers/script.php'
-?>
-
 <!doctype html>
 <html lang='en'>
 
 <head>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <title>Disc dispense</title>
+
+    <!-- CSS -->
     <link href='./assets/css/style.css' rel='stylesheet'>
+
+    <title>Albums dispense</title>
 </head>
 
 <body>
 
-    <div id='app'>
-        <header>
-
-        </header>
+    <div id="app">
 
         <main>
+            <div class="dischi">
 
+                <div class="card" v-for="album in albums">
+                    <img :src="album.poster" alt="">
+                    <div>{{album.title}}</div>
+                </div>
+
+            </div>
         </main>
-
-        <footer>
-
-        </footer>
     </div>
     <!-- /.app -->
 
+    <!-- AXIOS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js" integrity="sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <!-- Development only cdn, disable in production -->
+
+    <!-- VUE 3 -->
     <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+
+    <!-- JS -->
     <script src='./assets/js/app.js'></script>
 </body>
 
