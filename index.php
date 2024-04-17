9d278a65fad8d29d93863e206ed07ec70f36302e
MarioBroi <inoix16@gmail.com>
2024-04-17 13:50:23 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+echo "Test php";
+?>
+
+<!doctype html>
+<html lang='en'>
+
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>Disc dispense</title>
+    <link href='./assets/css/style.css' rel='stylesheet'>
+</head>
+
+<body>
+
+    <div id='app'>
+        <header>
+
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+    </div>
+    <!-- /.app -->
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js" integrity="sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- Development only cdn, disable in production -->
+    <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+    <script src='./app.js'></script>
+</body>
+
+</html>
