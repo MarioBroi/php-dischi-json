73aab4971d310c7a471ab49bd6b55cf8ff172803
MarioBroi <inoix16@gmail.com>
2024-04-17 13:54:00 +0100

    edit script search to reach the correct folder

--- index.php
+++ index.php
@@ -28,11 +28,11 @@ echo "Test php";
         </footer>
     </div>
     <!-- /.app -->
-    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js" integrity="sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- Development only cdn, disable in production -->
     <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
-    <script src='./app.js'></script>
+    <script src='./assets/js/app.js'></script>
 </body>
 
 </html>
